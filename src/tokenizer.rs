342aec7351618a2f9f27fd84a9581c169f1e9b62
Madison Huber <hubermm@live.unc.edu>
2019-03-02 03:06:44 +0000

    Establish a Tokenizer struct and new method

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -35,3 +35,36 @@ pub enum Token {
     Char(char),
 }
 
+/**
+ * The internal state of a Tokenizer is maintained by a peekable character
+ * iterator over a &str's Chars.
+ */
+pub struct Tokenizer<'str> {
+    chars: Peekable<Chars<'str>>,
+}
+
+impl<'str> Tokenizer<'str> {
+    pub fn new(input: &'str str) -> Tokenizer {
+        Tokenizer {
+            chars: input.chars().peekable(),
+        }
+    }
+}
+
+/**
+ * The Iterator trait is implemented for Tokenizer. It will produce items of 
+ * type Token and has a `next` method that returns Option<Token>.
+ */
+impl<'str> Iterator for Tokenizer<'str> {
+    type Item = Token;
+
+    /**
+     * The `next` method ignores leading whitespace and returns the next
+     * complete Some(Token) in the Tokenizer's input string or None at all.
+     */
+    fn next(&mut self) -> Option<Token> {
+        self.lex_whitespace(); // implement this!
+        // continue implementing
+    }
+}
+
