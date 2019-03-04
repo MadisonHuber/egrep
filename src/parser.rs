b595789283174f36d18f6df765d88b2aca6e2c7e
Alana Fiordalisi <fiordali@live.unc.edu>
2019-03-04 02:00:36 +0000

    Sets up Parser, atom

--- src/parser.rs
+++ src/parser.rs
@@ -15,6 +15,7 @@ use std::iter::Peekable;
  */
 
 /* == Begin Syntax Tree Elements == */
+#[derive(Debug, PartialEq)]
 pub enum AST {
     Alternation(Box<AST>, Box<AST>),
     Catenation(Box<AST>, Box<AST>),
@@ -22,3 +23,111 @@ pub enum AST {
     Char(char),
     AnyChar,
 }
+
+/* == End Syntax Tree Elements == */
+
+pub struct Parser<'tokens> {
+    tokens: Peekable<Tokenizer<'tokens>>,
+}
+
+impl<'tokens> Parser<'tokens> {
+    pub fn parse(tokenizer: Tokenizer<'tokens>) -> Result<AST, String> {
+        let mut parser = Parser {
+            tokens: tokenizer.peekable(),
+        };
+
+        // Parse, and then ensure that all tokens in the expr were used.
+        // Extra tokens causes error, else the structure (usually binop tree) is returned.
+        let result = parser.reg_expr();
+        if let Some(c) = parser.tokens.next() {
+            Err(format!("Expected end of input, found {:?}", c))
+        } else {
+            result
+        }
+    }
+}
+
+/**
+ * Internal-only parser methods to process grammar through recursive descent.
+ */
+
+impl<'tokens> Parser<'tokens> {
+    // RegExpr -> atom
+    fn reg_expr(&mut self) -> Result<AST, String> {
+        self.atom()
+    }
+    
+    // Atom -> LParen <RegExpr> RParen | AnyChar | Char
+    fn atom(&mut self) -> Result<AST, String> {
+        Ok(AST::AnyChar)
+    }
+}
+
+/**
+ * Tests for helper methods.
+ */
+
+#[cfg(test)]
+mod private_api {
+    use super::*;
+
+    mod lvl0 {
+        use super::*;
+
+        #[test]
+        fn atom() {
+            assert_eq!(Parser::from(".").atom().unwrap(), AST::AnyChar);
+        }
+    }
+
+}
+
+/* Parser's Helper Methods to improve ergonomics of parsing */
+impl<'tokens> Parser<'tokens> {
+    /**
+     * Static helper method used in unit tests to establish a
+     * parser given a string.
+     */
+    fn from(input: &'tokens str) -> Parser<'tokens> {
+        Parser {
+            tokens: Tokenizer::new(input).peekable(),
+        }
+    }
+
+    /**
+     * When you expect another token and want to take it directly
+     * or raise an error that you expected another token here but
+     * found the end of input. Example usage:
+     *
+     * let t: Token = self.take_next_token()?;
+     *
+     * Notice the ? usage will automatically propagate the Err or
+     * unwrap the value of Ok.
+     */
+    fn take_next_token(&mut self) -> Result<Token, String> {
+        if let Some(token) = self.tokens.next() {
+            Ok(token)
+        } else {
+            Err(String::from("Unexpected end of input"))
+        }
+    }
+
+    /**
+     * When there's a specific token you expect next in the grammar
+     * use this helper method. It will raise an Err if there is no
+     * next token or if it is not _exactly_ the Token you expected
+     * next. If it is the token you expected, it will return Ok(Token).
+     */
+    fn consume_token(&mut self, expected: Token) -> Result<Token, String> {
+        if let Some(next) = self.tokens.next() {
+            if next != expected {
+                Err(format!("Expected: {:?} - Found {:?}", expected, next))
+            } else {
+                Ok(next)
+            }
+        } else {
+            Err(String::from("Unexpected end of input"))
+        }
+    }
+}
+
