6ed00a4744e536000dd601fbc1037950ebb7ea2d
Madison Huber <hubermm@live.unc.edu>
2019-04-23 19:56:06 +0000

    Add comments for gen

--- src/nfa/helpers.rs
+++ src/nfa/helpers.rs
@@ -55,11 +55,25 @@ impl std::fmt::Display for Char {
 use rand::prelude::*;
 use rand::distributions::Alphanumeric;
 
+/**
+ * Generate a specified number of random strings that will be accepted by the given nfa. Takes in a
+ * reference to an NFA and a usize which will be how many strings are generated.
+ * Returns a Vec<String> containing all the generated strings.
+ */
 pub fn gen(nfa: &NFA, num: usize) -> Vec<String> {
+    // create the vector that will contain the generated strings and that will be returned
     let mut strings: Vec<String> = Vec::new();
+
+    // loop until the vector contains the specified number of strings
     while strings.len() < num {
+        // make the string that will be built up to eventually become the accepted string
         let mut s = String::new();
+
+        // idx is the index of the current state in the nfa, start at 0 which is the start state
         let mut idx = 0;
+
+        // loop infinitely and only break when we are at the end state
+        // go through the whole nfa state by state from start to end
         loop {
             match &nfa.states[idx] {
                 Start(Some(id)) => idx = *id,
@@ -68,12 +82,14 @@ pub fn gen(nfa: &NFA, num: usize) -> Vec<String> {
                     if let Char::Literal(ch) = &c {
                         s.push(*ch);
                     } else {
-                        // push random char since any
+                        // otherwise push a random char since we can take any character
                         s.push(thread_rng().sample(Alphanumeric));
                     }
                     idx = *id;
                 },
                 Split(Some(lhs), Some(rhs)) => {
+                    // use random() to generate a random bool
+                    // in order to randomly decide which branch of the split to follow
                     if random() {
                         idx = *lhs;
                     } else {
@@ -81,13 +97,20 @@ pub fn gen(nfa: &NFA, num: usize) -> Vec<String> {
                     }
                 },
                 End => {
+                    // we've reached the end of the nfa
+                    // so push the built-up string onto the strings vec
                     strings.push(s);
-                    break; // exits loop
+                    
+                    // made it through the nfa, so break out of the loop
+                    break;
                 },
-                _ => break,
+                _ => { /* default case so match is comprehensive
+                          don't want to do anthing here
+                          since we should never reach this point */ },
             }
         }
     }
+    // return the vector containing all the randomly generated strings
     strings
 }
 
@@ -160,9 +183,8 @@ mod gen_tests {
 
     #[test]
     fn gen_7_alt_plus() {
-        // let nfa = NFA::from("(a*|bc)+").unwrap();
         let nfa = NFA::from("(a|b)+").unwrap();
-        let strings = gen(&nfa, 3);
+        let strings = gen(&nfa, 7);
         for st in &strings {
             assert!(nfa.accepts(st));
         }
@@ -185,17 +207,4 @@ mod gen_tests {
             assert!(nfa.accepts(st));
         }
     }
-
-
-    /*
-    #[test]
-    fn gen_5_empty() {
-        let nfa = NFA::from("").unwrap();
-        println!("{:?}", nfa);
-        let strings = gen(&nfa, 5);
-        for st in &strings {
-            assert!(nfa.accepts(st));
-        }
-    }
-    */
 }
